033e751b1788d78998f900a696758b68ee45b9fe
Ishan Arya <ishanarya0@gmail.com>
2024-09-30 15:25:20 +0530

    feat: add flink module (#110)
    * rebase
    
    * test: add e2e-test
    
    * test: fix TearDown
    
    * test: fix test
    
    * feat: change var name
    
    * feat: add dagger module create & update action (#107)
    
    * feat: add dagger module
    
    * feat: add flink dep
    
    * feat: add transformations
    
    * fix: read stream from config root
    
    * feat: add Plan implementation
    
    * fix: chart values
    
    * fix: resolve TODOs and refactored
    
    * fix: source sink base handling
    
    * feat: Output to have CR details
    
    * feat: handle status
    
    * refactor: seperate contants by type
    
    * refactor: kubeGetCRD function
    
    * feat: add dagger update action
    
    * fix: add Update action
    
    * chore: change var name to sink_kafka_stream
    
    * feat: merge consumer group ID if sink is same
    
    ---------
    
    Co-authored-by: Ishan Arya <ishan.arya@gojek.com>
    
    * feat: dagger actions (#114)
    
    * feat: add dagger module
    
    * feat: add flink dep
    
    * feat: add transformations
    
    * fix: read stream from config root
    
    * feat: add Plan implementation
    
    * fix: chart values
    
    * fix: resolve TODOs and refactored
    
    * fix: source sink base handling
    
    * feat: Output to have CR details
    
    * feat: handle status
    
    * refactor: seperate contants by type
    
    * refactor: kubeGetCRD function
    
    * feat: add dagger update action
    
    * fix: add Update action
    
    * chore: change var name to sink_kafka_stream
    
    * feat: merge consumer group ID if sink is same
    
    * feat: add start, stop and reset actions (#112)
    
    * feat: add start & update action
    
    * feat: add reset action
    
    ---------
    
    Co-authored-by: Ishan Arya <ishan.arya@gojek.com>
    
    ---------
    
    Co-authored-by: Ishan Arya <ishan.arya@gojek.com>
    
    * feat: add extra streams
    
    ---------
    
    Co-authored-by: Ishan Arya <ishan.arya@gojek.com>

---
+++ modules/dagger/module.go
@@ -0,0 +1,156 @@
+package dagger
+
+import (
+	"context"
+	_ "embed"
+	"encoding/json"
+	"time"
+
+	"github.com/goto/entropy/core/module"
+	"github.com/goto/entropy/modules/flink"
+	"github.com/goto/entropy/pkg/errors"
+	"github.com/goto/entropy/pkg/helm"
+	"github.com/goto/entropy/pkg/kube"
+	"github.com/goto/entropy/pkg/validator"
+	"helm.sh/helm/v3/pkg/release"
+	v1 "k8s.io/api/core/v1"
+)
+
+const (
+	keyFlinkDependency = "flink"
+	StopAction         = "stop"
+	StartAction        = "start"
+	ResetAction        = "reset"
+)
+
+type FlinkCRDStatus struct {
+	JobManagerDeploymentStatus string `json:"jobManagerDeploymentStatus"`
+	JobStatus                  string `json:"jobStatus"`
+	ReconciliationStatus       string `json:"reconciliationStatus"`
+}
+
+var Module = module.Descriptor{
+	Kind: "dagger",
+	Dependencies: map[string]string{
+		keyFlinkDependency: flink.Module.Kind,
+	},
+	Actions: []module.ActionDesc{
+		{
+			Name:        module.CreateAction,
+			Description: "Creates a new dagger",
+		},
+		{
+			Name:        module.UpdateAction,
+			Description: "Updates an existing dagger",
+		},
+		{
+			Name:        StopAction,
+			Description: "Suspends a running dagger",
+		},
+		{
+			Name:        StartAction,
+			Description: "Starts a suspended dagger",
+		},
+		{
+			Name:        ResetAction,
+			Description: "Resets the offset of a dagger",
+		},
+	},
+	DriverFactory: func(confJSON json.RawMessage) (module.Driver, error) {
+		conf := defaultDriverConf // clone the default value
+		if err := json.Unmarshal(confJSON, &conf); err != nil {
+			return nil, err
+		} else if err := validator.TaggedStruct(conf); err != nil {
+			return nil, err
+		}
+
+		return &daggerDriver{
+			conf:    conf,
+			timeNow: time.Now,
+			kubeDeploy: func(_ context.Context, isCreate bool, kubeConf kube.Config, hc helm.ReleaseConfig) error {
+				canUpdate := func(rel *release.Release) bool {
+					curLabels, ok := rel.Config[labelsConfKey].(map[string]any)
+					if !ok {
+						return false
+					}
+					newLabels, ok := hc.Values[labelsConfKey].(map[string]string)
+					if !ok {
+						return false
+					}
+
+					isManagedByEntropy := curLabels[labelOrchestrator] == orchestratorLabelValue
+					isSameDeployment := curLabels[labelDeployment] == newLabels[labelDeployment]
+
+					return isManagedByEntropy && isSameDeployment
+				}
+
+				helmCl := helm.NewClient(&helm.Config{Kubernetes: kubeConf})
+				_, errHelm := helmCl.Upsert(&hc, canUpdate)
+				return errHelm
+			},
+			kubeGetPod: func(ctx context.Context, conf kube.Config, ns string, labels map[string]string) ([]kube.Pod, error) {
+				kubeCl, err := kube.NewClient(ctx, conf)
+				if err != nil {
+					return nil, errors.ErrInternal.WithMsgf("failed to create new kube client on firehose driver kube get pod").WithCausef(err.Error())
+				}
+				return kubeCl.GetPodDetails(ctx, ns, labels, func(pod v1.Pod) bool {
+					// allow pods that are in running state and are not marked for deletion
+					return pod.Status.Phase == v1.PodRunning && pod.DeletionTimestamp == nil
+				})
+			},
+			kubeGetCRD: func(ctx context.Context, conf kube.Config, ns string, name string) (kube.FlinkDeploymentStatus, error) {
+				kubeCl, err := kube.NewClient(ctx, conf)
+				if err != nil {
+					return kube.FlinkDeploymentStatus{}, errors.ErrInternal.WithMsgf("failed to create new kube client on firehose driver kube get pod").WithCausef(err.Error())
+				}
+				crd, err := kubeCl.GetCRDDetails(ctx, ns, name)
+				if err != nil {
+					return kube.FlinkDeploymentStatus{}, err
+				}
+				return parseFlinkCRDStatus(crd.Object)
+			},
+			consumerReset: consumerReset,
+		}, nil
+	},
+}
+
+func parseFlinkCRDStatus(flinkDeployment map[string]interface{}) (kube.FlinkDeploymentStatus, error) {
+	var flinkCRDStatus FlinkCRDStatus
+	statusInterface, ok := flinkDeployment["status"].(map[string]interface{})
+	if !ok {
+		return kube.FlinkDeploymentStatus{}, errors.ErrInternal.WithMsgf("failed to convert flink deployment status to map[string]interface{}")
+	}
+
+	if jmStatus, ok := statusInterface["jobManagerDeploymentStatus"].(string); ok {
+		flinkCRDStatus.JobManagerDeploymentStatus = jmStatus
+	}
+	if jobStatus, ok := statusInterface["jobStatus"].(map[string]interface{}); ok {
+		if st, ok := jobStatus["state"].(string); ok {
+			flinkCRDStatus.JobStatus = st
+		}
+	}
+	if reconciliationStatus, ok := statusInterface["reconciliationStatus"].(map[string]interface{}); ok {
+		if st, ok := reconciliationStatus["state"].(string); ok {
+			flinkCRDStatus.ReconciliationStatus = st
+		}
+	}
+
+	status := kube.FlinkDeploymentStatus{
+		JMDeployStatus: flinkCRDStatus.JobManagerDeploymentStatus,
+		JobStatus:      flinkCRDStatus.JobStatus,
+		Reconciliation: flinkCRDStatus.ReconciliationStatus,
+	}
+	return status, nil
+}
+
+func consumerReset(ctx context.Context, conf Config, resetTo string) []Source {
+	baseGroup := conf.Source[0].SourceKafkaConsumerConfigGroupID
+	groupId := incrementGroupId(baseGroup, len(conf.Source))
+
+	for i := range conf.Source {
+		conf.Source[i].SourceKafkaConsumerConfigGroupID = incrementGroupId(groupId, i)
+		conf.Source[i].SourceKafkaConsumerConfigAutoOffsetReset = resetTo
+	}
+
+	return conf.Source
+}
