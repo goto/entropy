038ef6e4cda7ab6b99851f1f3fd8ce0e345b497c
Ishan Arya <ishan.arya@gojek.com>
2024-10-08 13:28:45 +0530

    fix: flink config check

--- modules/flink/driver_plan.go
+++ modules/flink/driver_plan.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/goto/entropy/core/module"
 	"github.com/goto/entropy/core/resource"
+	"github.com/goto/entropy/modules"
 )
 
 func (fd *flinkDriver) Plan(ctx context.Context, res module.ExpandedResource, act module.ActionRequest) (*resource.Resource, error) {
@@ -12,6 +13,13 @@ func (fd *flinkDriver) Plan(ctx context.Context, res module.ExpandedResource, ac
 		Configs:      act.Params,
 		Dependencies: res.Spec.Dependencies,
 	}
+
+	conf, err := readConfig(res.Resource.Spec.Configs, fd.conf)
+	if err != nil {
+		return nil, err
+	}
+
+	res.Resource.Spec.Configs = modules.MustJSON(conf)
 
 	output, err := fd.Output(ctx, res)
 	if err != nil {
